bd7d42cf35a515b565c8ea66c4bef5c608e158d8
dainyzyg <470067721@qq.com>
2018-03-14 19:24:40 -0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .page
+    .c(v-show="select=='1'")
+      h1(v-for="i in 'sdadqwdasddsfwfqdsadqdsad'") {{i}}
+    .c(v-show="select=='2'")
+      h1(v-for="i in 20") {{i}}
+    mt-tabbar(v-model="select")
+      mt-tab-item(id="1")
+        i.icon.iconfont.icon-createtask(slot="icon")
+        | 订单
+      mt-tab-item(id="2")
+        i.icon.iconfont.icon-clock(slot="icon")
+        | 进行中
+      mt-tab-item(id="3")
+        i.icon.iconfont.icon-addressbook(slot="icon")
+        | 我的
 </template>
 
 <script>
@@ -12,17 +23,22 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      select: '1'
+    }
+  },
+  methods: {}
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+i.icon.iconfont {
+  font-size: 26px;
+}
+.c {
+  height: 70vh;
+  overflow: auto;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <img src="pic/timg.jpg">
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
